9722f457b511d95212f33242118bdaac42f59ec3
devop9895 <163037755+devop9895@users.noreply.github.com>
2026-04-09 11:36:26 -0300

    wip input filter with useSearchParams react-router-dom

--- src/features/shopping/components/Filter.tsx
+++ src/features/shopping/components/Filter.tsx
@@ -1,25 +1,22 @@
 import { type SyntheticEvent, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 const QUERY_PARAM = 'query';
 
 export function Filter() {
-  const { search } = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const getInitialQuery = (): string => {
-    const urlParams = new URLSearchParams(search);
-    return urlParams.get(QUERY_PARAM) || '';
+    return searchParams.get(QUERY_PARAM) || '';
   };
   const [query, setQuery] = useState<string>(getInitialQuery);
 
-  const navigate = useNavigate();
   const handleChange = (event: SyntheticEvent) => {
     const { value } = event.target;
     setQuery(value);
 
-    const urlParams = new URLSearchParams(search);
-    urlParams.set(QUERY_PARAM, value);
-
-    navigate(`/shopping?${urlParams.toString()}`);
+    searchParams.set(QUERY_PARAM, value);
+    setSearchParams(searchParams);
   };
 
   return (
